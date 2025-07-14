e2ef31f09338ebae1cb75f974569ec042cfd4ca4
coincidence <114846702+coincidence-web@users.noreply.github.com>
2025-07-14 23:35:27 +0700

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 // config.js
+const SMARTLINKS = [
+  "https://www.profitableratecpm.com/gcwpbm2im?key=3745b60b814251e205062113bf24c921",
+  "https://otieu.com/4/9569226"
+];
+
+// random
 const CONFIG = {
-  smartlink: "https://your-smartlink.com/reward",  // smartlink 
+  smartlink: SMARTLINKS[Math.floor(Math.random() * SMARTLINKS.length)],
   countdownSeconds: 9
 };
