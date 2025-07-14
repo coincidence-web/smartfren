fe24e8408cee13fc8f08ffba35ea6298265a632f
coincidence <114846702+coincidence-web@users.noreply.github.com>
2025-07-14 23:47:28 +0700

    rf_data.js

--- rf_data.js
+++ rf_data.js
@@ -1,10 +1,16 @@
 // rf_data.js
 const SPOOF_REFERER = {
   list: [
-    "https://www.google.com",
-    "https://www.facebook.com",
-    "https://twitter.com",
-    "https://www.reddit.com"
+  "https://coinmarketcap.com/airdrop/",
+  "https://dappradar.com/quest",
+  "https://zealy.io/c/crypto-community/questboard",
+  "https://galxe.com/quest",
+  "https://faucetpay.io/faucet-list",
+  "https://www.binance.com/en/giveaway",
+  "https://www.okx.com/jumpstart",
+  "https://community.trustwallet.com/c/announcements",
+  "https://www.reddit.com/r/cryptocurrencydeals/",
+  "https://www.coingecko.com/account/candy"
   ]
 };
 
